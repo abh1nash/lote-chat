6e19b55c42bf743de577b77deee7ba5a612cec86
abhinash <best.abhin@yahoo.com>
2019-09-13 14:08:49 +0545

    Clean Up

--- src/router.js
+++ src/router.js
@@ -1,17 +1,17 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      name: "login",
+      component: Login
     },
     {
       path: "/about",
@@ -23,4 +23,6 @@ export default new Router({
         import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
-});
+})
+
+export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container flex-center">
+    <div class="card login-card">
+      <h2>Login</h2>
+      <form>
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Your Username *" value />
+        </div>
+        <div class="form-group">
+          <input type="password" class="form-control" placeholder="Your Password *" value />
+        </div>
+        <div class="form-group">
+          <button type="submit" class="btn btn-primary">Login</button>
+        </div>
+        <div class="form-group">
+          <a href="#" class="ForgetPwd">Forgot Password?</a>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.flex-center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+.login-card {
+  box-sizing: border-box;
+  padding: 30px;
+  width: 400px;
+  text-align: center;
+  border-radius: 20px;
+  border: none;
+  box-shadow: 0 10px 15px 0px #007bff;
+
+  input {
+    border-radius: 10px;
+    border: none;
+    background: #eee;
+    box-shadow: none;
+
+    &:focus {
+      outline: none;
+      border: none;
+      box-shadow: 0 1px 3px 0px #007bff;
+    }
+  }
+}
+</style>
